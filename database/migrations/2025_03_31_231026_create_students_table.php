b35b79d95d1b4bc64f8514e00444a369a768b696
amer reslan <amerreslan13@gmail.com>
2025-04-02 09:01:05 +0300

    Role based middleware added

--- database/migrations/2025_03_31_231026_create_students_table.php
+++ database/migrations/2025_03_31_231026_create_students_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('full_name');
             $table->string('email')->unique();
+            $table->string('role')->default('student');
             $table->string('password');
             $table->timestamps();
             
--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,19 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/register', [App\Http\Controllers\authController::class, 'register']);
 Route::post('/login', [App\Http\Controllers\authController::class, 'login']);
-Route::post('/logout', [App\Http\Controllers\authController::class, 'logout'])->middleware('auth:sanctum');
+
+
+Route::middleware('auth:sanctum')->group(function (){
+    //here we will find the routes that needs to be login inorder to access
+    Route::post('/logout', [App\Http\Controllers\authController::class, 'logout']);
+
+
+
+    Route::middleware('checkRole')->group(function(){
+        //here we will define the routes that need to be admin to access
+
+    });
+    
+
+});
 
